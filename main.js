e758065702e2434d779f48eb120dc5b6da236a4b
robertcoco <79550572+robertcoco@users.noreply.github.com>
2022-06-09 15:01:55 -0400

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var boton = document.getElementById("button");
-function elemento(){
-    var elemento = document.getElementsByClassName("contenedor")
-    var item = elemento.querySelector(".notes");
-    elemento.removeChild(item);
-}
-boton.addaddEventListener("onclick",elemento());
